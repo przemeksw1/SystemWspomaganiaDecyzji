d03128595551b94c044b4bbe98ffc1762e9c4208
Kasia S <123kasia123@gmail.com>
2020-11-04 23:48:00 +0100

    alfabetic and key

--- SystemWspomaganiaDecyzji/App.xaml.cs
+++ SystemWspomaganiaDecyzji/App.xaml.cs
@@ -17,7 +17,9 @@ namespace SystemWspomaganiaDecyzji
         public App()
         {
             // Set this code once in App.xaml.cs or application startup
-            SciChartSurface.SetRuntimeLicenseKey("WGa8guRPIDY8ZXV+HFFiSOiY6O1ldsqNK7RpU2SGIV7GQGbpA5on5PiwaJ4sSd34WLNvJdVWPA8rnIvzOgYezXIfzigrXgtVzQnEZqOYZfRYaaxCktdYQvX0rOqHrkhJTuTjSq9Y5RV6bUr8kTiBQpJx2nn/yHhh2VImt2jcjlTZDufrROFmSebni8ncMlApXVVrwvh/LoOleFFFgTNlqIclNOtDQz7iTYBFgFXjkYl+6Qj7v+BAJoAs+AB0xOJ85UvU8esuAYuYOGvPvjQN4/AhPmCNdBAYDirIMwADCSomPCB6nSyDrT67byl96g39JLijciZKPyNRz+8QAufajD4E96IUTcrxR6/ykiStDvwAIKWcZWxGdoRFS1o94TkJc6XFJl1A2e41RFaM3UFnNnuBNNjkL4CAno4Ms60Zhw5Fp9JmVacGlIGrshQ7P3vkkMRfkjH4QOlhuLEIxFBVDpeqIcn2gnbCq5NoS98aziSeIurCFqwjsZ+Ktb0KFbHWXs7Eb42zoqT7e5OO+Cz5Ih2TIG+wUKHk8ZKm5z/ZfGLdPqXev/P9yfy80x9sGb6Z4HDHnEeQ/Bug3uo=");
+            // Set this code once in App.xaml.cs or application startup
+            SciChartSurface.SetRuntimeLicenseKey("ZhzFT5OKfqY6sIs/ohhKe/5TllVe2k3h+xWXtcs9zTQElU2d/RTJppZawQU8YiBzr828958rhN98GCURIrM+AZ2SIhn/OMj8V6hzXEs26iMumuM//F1hPSdfRhIw31wLC+uhdsTXU8d8s32rPG6GPrYWa9zBoHJUPGJfujE/DKlKsEanfNTH9oiJd/AkAJqzDurxRqk8IfF2rB69poljK5qtI7Qf7nswT92+uz6B6cYfwIU6kU3MaqDdn3bz+7uSws08A2ChgATcPdocQGDn/4V7U4RRAiftlCQXTsJmvx+qUUNyo2/ZC7y4iVi5r2uUIuoRRWlKrkRgPj0bKgu7Db8K6FOPgZxkso9W0iduEw1CJ2fvW7YM6FdZGpqr3V4slmI6oxxmBh5xgntknrHVI+RSsJbv0LFOo2I0+GsQ8pBNsjEQXziMLb7X3eVGYZsrTsgPoI1Yqm5MZ9YN5+cWeWHViHOzLe4WftxLlMDoywT/ERX0phwOGKX0/pp9Lgeyn4/UKn5e2cZXroxrklMtISzaD0hk7cGfBbLva2dC6FAFXMfh2Ejde2QL+tsbTLmJ2eiQDqPpQUw+zCo=");
+            //SciChartSurface.SetRuntimeLicenseKey("WGa8guRPIDY8ZXV+HFFiSOiY6O1ldsqNK7RpU2SGIV7GQGbpA5on5PiwaJ4sSd34WLNvJdVWPA8rnIvzOgYezXIfzigrXgtVzQnEZqOYZfRYaaxCktdYQvX0rOqHrkhJTuTjSq9Y5RV6bUr8kTiBQpJx2nn/yHhh2VImt2jcjlTZDufrROFmSebni8ncMlApXVVrwvh/LoOleFFFgTNlqIclNOtDQz7iTYBFgFXjkYl+6Qj7v+BAJoAs+AB0xOJ85UvU8esuAYuYOGvPvjQN4/AhPmCNdBAYDirIMwADCSomPCB6nSyDrT67byl96g39JLijciZKPyNRz+8QAufajD4E96IUTcrxR6/ykiStDvwAIKWcZWxGdoRFS1o94TkJc6XFJl1A2e41RFaM3UFnNnuBNNjkL4CAno4Ms60Zhw5Fp9JmVacGlIGrshQ7P3vkkMRfkjH4QOlhuLEIxFBVDpeqIcn2gnbCq5NoS98aziSeIurCFqwjsZ+Ktb0KFbHWXs7Eb42zoqT7e5OO+Cz5Ih2TIG+wUKHk8ZKm5z/ZfGLdPqXev/P9yfy80x9sGb6Z4HDHnEeQ/Bug3uo=");
         }
     }
 }
